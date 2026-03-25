e998219edb4ffe91dbbcd55de723766d2e0a661e
BruCandy <161817534+BruCandy@users.noreply.github.com>
2026-03-25 09:25:41 +0900

    added test2.cpp

---
+++ test2.cpp
@@ -0,0 +1,101 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+const uint8_t ROWS = 6;
+const uint8_t COLS = 3;
+
+const uint8_t rowPins[ROWS] = {10, 16, 14, 15, 18, 19};
+const uint8_t colPins[COLS] = {9, 8, 7};
+
+bool prevState[ROWS][COLS];
+
+const int led = 3;
+Adafruit_NeoPixel pixel(1, led, NEO_GRB + NEO_KHZ800);
+
+const int VERT_PIN = A7;
+const int HORZ_PIN = A6;
+
+char keymap[ROWS][COLS] = {
+  {'_', 'a', 'b'},
+  {'c', 'd', 'e'},
+  {'f', 'g', 'h'},
+  {'i', 'j', '_'},
+  {'k', 'l', 'm'},
+  {'n', 'o', 'p'}
+};
+
+void setup() {
+  delay(3000);
+
+  pixel.begin();
+  pixel.setPixelColor(0, pixel.Color(255, 0, 0));
+  pixel.show();
+
+  pinMode(VERT_PIN, INPUT);
+  pinMode(HORZ_PIN, INPUT);
+
+  Serial.begin(115200);
+
+  for (uint8_t r = 0; r < ROWS; r++) {
+    pinMode(rowPins[r], OUTPUT);
+    digitalWrite(rowPins[r], HIGH);
+  }
+
+  for (uint8_t c = 0; c < COLS; c++) {
+    pinMode(colPins[c], INPUT_PULLUP);
+  }
+
+  for (uint8_t r = 0; r < ROWS; r++) {
+    for (uint8_t c = 0; c < COLS; c++) {
+      prevState[r][c] = false;
+    }
+  }
+}
+
+void scanMatrix() {
+  for (uint8_t r = 0; r < ROWS; r++) {
+    for (uint8_t i = 0; i < ROWS; i++) {
+      digitalWrite(rowPins[i], HIGH);
+    }
+
+    digitalWrite(rowPins[r], LOW);
+    delayMicroseconds(5);
+
+    for (uint8_t c = 0; c < COLS; c++) {
+      bool pressed = (digitalRead(colPins[c]) == LOW);
+
+      if (pressed != prevState[r][c]) {
+        prevState[r][c] = pressed;
+
+        Serial.print("row=");
+        Serial.print(r);
+        Serial.print(" col=");
+        Serial.print(c);
+        Serial.print(" key=");
+        Serial.print(keymap[r][c]);
+        Serial.println(pressed ? " pressed" : " released");
+      }
+    }
+  }
+}
+
+void scanJoicon() {
+  int vert = analogRead(VERT_PIN);
+  int horz = analogRead(HORZ_PIN);
+  if (horz < 300) {
+    Serial.print("→");
+  } else if (horz > 700) {
+    Serial.print("←");
+  }
+  if (vert < 300) {
+    Serial.print("↓");
+  } else if (vert > 700) {
+    Serial.print("↑");
+  }
+}
+
+void loop() {
+  scanMatrix();
+  scanJoicon();
+  delay(10);
+}
